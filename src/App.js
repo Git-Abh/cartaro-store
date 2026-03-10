47b3c6eb4cbe5ae1c395bdd4e24aeb12d263a9a4
abhinav k <abhinak2413@gmail.com>
2026-03-10 08:15:44 +0000

    fix: remove duplicate closing in search

--- src/App.js
+++ src/App.js
@@ -3491,7 +3491,6 @@ export default function App() {
                     </div>
                   </div>
               )}
-              )}
             </div>
             {/* Wishlist */}
             <button
