b5f494874181e9216f5f84d5f547865bac3129cf
abhinav k <abhinak2413@gmail.com>
2026-03-12 18:05:24 +0000

    fix: import React for AnalyticsTab hooks

--- src/AdminPage.js
+++ src/AdminPage.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { db } from "./firebase";
 import { collection, getDocs, orderBy, query, doc, updateDoc, addDoc, deleteDoc } from "firebase/firestore";
 
